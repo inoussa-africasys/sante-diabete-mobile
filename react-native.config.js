18ae7fa63316a1f2d1864b8f4bbffd7d4484d742
Inoussa ZERBO <inoussa.zerbo@africasys.com>
2025-09-19 09:29:57 +0000

    refactor: reorder SQLite and edge-to-edge config properties in gradle.properties

---
+++ react-native.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    dependencies: {
+      'react-native-zip-archive': {},
+      '@react-native-async-storage/async-storage': {},
+      '@react-native-community/datetimepicker': {},
+      '@react-native-picker/picker': {},
+      'react-native-gesture-handler': {},
+      'react-native-reanimated': {},
+      'react-native-safe-area-context': {},
+      'react-native-screens': {},
+      'react-native-svg': {},
+      'react-native-webview': {},
+      'react-native-edge-to-edge': {}, // si tu l’utilises vraiment
+    },
+  };
